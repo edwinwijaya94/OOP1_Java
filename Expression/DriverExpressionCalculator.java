d1c11185fbc41cee88c471c924a9415a87809919
edwinwijaya94 <edwinwijaya94@yahoo.com>
2015-04-08 11:24:10 +0700

    javadoc DriverExpressionCalculator

--- Expression/DriverExpressionCalculator.java
+++ Expression/DriverExpressionCalculator.java
@@ -1,6 +1,20 @@
 package Expression;
 import java.util.*;
+/**
+ * DriverExpressionCalculator adalah kelas yang 
+ * berfungsi sebagai driver dari package expression,
+ * menerima input ekspresi dari user.
+ * 
+ * @author Edwin Wijaya
+ * 
+*/
 public class DriverExpressionCalculator{
+	/**
+	 * method Main untuk menerima input ekspresi dan mencetak
+	 * ke layar.
+	 * @param -
+	 * @see ExpressionCalculator#calculate(Object) calculate
+	*/
 	public static void main (String[] args){
 		String expression = new Scanner(System.in).nextLine();		;
 		System.out.println(ExpressionCalculator.calculate(expression));
