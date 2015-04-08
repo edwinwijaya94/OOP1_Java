431f8214b620b1bce496f70d46d630fb36dfa402
elvanowen <vanzz_95@hotmail.com>
2015-04-08 11:17:57 +0700

    partially commented

--- commandProcessorWithDesignPattern/CalculateCommand.java
+++ commandProcessorWithDesignPattern/CalculateCommand.java
@@ -1,16 +1,28 @@
 package commandProcessorWithDesignPattern;
 
+/**
+ * The CalculateCommand class calculates a given expression
+ * @author Elvan Owen
+ */
 public class CalculateCommand implements Command
 {
 	private ICalculator calculator;
 	private String expression;
 	
+	/**
+	 * Constructor to initialize the expression and the calculator to run CalculateCommand
+	 * @param calculator  calculator object which will be used to calculate
+	 * @param expression  expression that is going to be calculated
+	 */
 	public CalculateCommand(ICalculator calculator,String expression)
 	{
 		this.calculator = calculator;
 		this.expression = expression;
 	}
 	
+	/**
+	 * This method will execute the given command using the given calculator
+	 */
 	public void execute()
 	{
 		calculator.calculate(expression);
--- commandProcessorWithDesignPattern/Command.java
+++ commandProcessorWithDesignPattern/Command.java
@@ -1,5 +1,9 @@
 package commandProcessorWithDesignPattern;
 
+/**
+ * The Command Interface makes sure every subclasses of Command have their own execute instructions
+ * @author Elvan Owen
+ */
 public interface Command {
 	public abstract void execute();
 }
